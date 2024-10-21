e15c0b04a7fdfbd1bbaccd9c4e5fde914da5c7bb
Victor <ukokjnr@gmail.com>
2024-10-21 08:52:17 +0100

    Fix: Paystack webhook.

--- routes/v1/paystackWebHook.ts
+++ routes/v1/paystackWebHook.ts
@@ -1,42 +1,85 @@
 import { Request, Response, Router, NextFunction } from "express";
 import axios from "axios";
-import { CustomRequest, CustomResponse } from "../../middleware/authenticatedUsersOnly";
+import {
+  CustomRequest,
+  CustomResponse,
+} from "../../middleware/authenticatedUsersOnly";
 import crypto from "crypto";
+import { eventTicketsBoughtCollection } from "../../models/EventTicketsBought";
+import { eventTicketTypeCollection } from "../../models/EventTicketTypes";
 
 const paystackRouter = Router();
 
-paystackRouter.post("/webhook", async (req: CustomRequest, res: CustomResponse, next: NextFunction) => {
+paystackRouter.post(
+  "/webhook",
+  async (req: CustomRequest, res: CustomResponse, next: NextFunction) => {
     try {
+      console.log(req.body.data);
 
-        console.log(req.body.data);
-
-        const hash = crypto.createHmac('sha512', process.env.PAYSTACK_SECRET_KEY as string).update(JSON.stringify(req.body)).digest('hex');
-        if (hash == req.headers['x-paystack-signature']) {
+      const hash = crypto
+        .createHmac("sha512", process.env.PAYSTACK_SECRET_KEY as string)
+        .update(JSON.stringify(req.body))
+        .digest("hex");
+      if (hash == req.headers["x-paystack-signature"]) {
         // Retrieve the request's body
         const event = req.body;
         console.log("Event", event);
 
-        
-        // Do something with event  
+        if (event.event == "charge.success" && event.data.status == "success") {
+          const ticketsBought =
+            await eventTicketsBoughtCollection.countDocuments({
+              paymentReference: event.data.reference,
+            });
+
+          const ticketDetails = await eventTicketsBoughtCollection.findOne({
+            paymentReference: event.data.reference,
+          });
+
+          await eventTicketsBoughtCollection.updateMany({
+            paymentReference: event.data.reference,
+          }, {
+            ticketStatus: "paid"
+          });
+
+          await eventTicketTypeCollection.findByIdAndUpdate(
+            ticketDetails?.ticketTypeId,
+            {
+              $inc: { totalTicketsBought: ticketsBought },
+            }
+          );
+
+          res.sendStatus(200);
+        } else {
+
+            await eventTicketsBoughtCollection.updateMany({
+                paymentReference: event.data.reference,
+              }, {
+                ticketStatus: "payment-failed"
+              });
+
+            res.sendStatus(400);
         }
-        res.send(200);
-
-        res.send(req.body.data); // originally res.send(200);
-
+      }
     } catch (error) {
-        next(error);
+      next(error);
     }
-});
+  }
+);
 
-paystackRouter.get("/bank-list", async (req: Request, res: Response, next: NextFunction) => {
-    const response = await axios.get("https://api.paystack.co/bank?country=nigeria", {
+paystackRouter.get(
+  "/bank-list",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const response = await axios.get(
+      "https://api.paystack.co/bank?country=nigeria",
+      {
         headers: {
-            Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`
-        }
-    });
+          Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
+        },
+      }
+    );
 
     res.send(response.data);
-
-});
+  }
+);
 
 export default paystackRouter;
